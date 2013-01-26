46196fac34ade058cae08f97455d27ee750ad377
David Drysdale <dmd@lurklurk.org>
2013-01-26 15:01:54 +0000

    empty framework files

---
+++ spec/mobiread_test.js
@@ -0,0 +1,7 @@
+describe("MobiRead", function() {
+    it("should complain of missing file", function() {
+        expect(true).toBe(true);
+    });
+    it("should complain of wrong file type", function() {
+    });
+});
