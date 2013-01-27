c57f511a9b451999fb713a40aac7ffcc60d043ad
David Drysdale <dmd@lurklurk.org>
2013-01-27 10:20:38 +0000

    First passing vestigial tests

--- spec/mobiread_test.js
+++ spec/mobiread_test.js
@@ -1,7 +1,32 @@
+
+var loadFileUrl = function (url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, false);  // synchronous
+    // retrieve data unprocessed as a binary string
+    xhr.overrideMimeType("text/plain; charset=x-user-defined");
+    xhr.send();
+    if (xhr.status === 200) {
+        return xhr.response; // Note: not xhr.responseText
+    } else {
+        throw new Error("File not found");
+    }
+}
+
 describe("MobiRead", function() {
     it("should complain of missing file", function() {
-        expect(true).toBe(true);
+        var loadMissing = function() {
+            data = loadFileUrl("nosuchfile");
+        }
+        expect(loadMissing).toThrow();
+    });
+    it("should load and parse a valid file", function() {
+        var data;
+        var loadIt = function() {
+            data = loadFileUrl("http://localhost/~dmd/current/jsebook/data/testbook.mobi");
+        }
+        expect(loadIt).not.toThrow();
     });
     it("should complain of wrong file type", function() {
     });
+
 });
