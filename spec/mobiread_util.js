cbed1ff5aefc17e4deb1b12d089bae0c0519f517
David Drysdale <dmd@lurklurk.org>
2013-12-31 16:25:04 +0000

    Move test utilities into separate file

---
+++ spec/mobiread_util.js
@@ -0,0 +1,31 @@
+var loadFileUrl = function (url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, false);  // synchronous
+    // retrieve data unprocessed as a binary string
+    xhr.overrideMimeType("text\/plain; charset=x-user-defined");
+    xhr.send(null);
+
+    if (xhr.status === 200) {
+        var bytes = [];
+        for (var ii=0; ii<xhr.response.length; ii++) {
+            bytes.push(xhr.responseText.charCodeAt(ii) & 0xFF);
+        }
+        return bytes;
+    } else {
+        throw new Error("File not found");
+    }
+}
+
+var addResult = function(htmlData) {
+    var container = document.getElementById('htmlresults');
+    if (container === null) {
+        container = document.createElement('div');
+        container.setAttribute('id', 'htmlresults');
+        document.body.appendChild(container);
+    }
+    var result = document.createElement('div');
+    result.setAttribute('class', 'htmlresult');
+    result.innerHTML = htmlData;
+    container.appendChild(document.createElement('hr'));
+    container.appendChild(result);
+}
