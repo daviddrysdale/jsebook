aefa446c068e07d3c2c9b65573f1e48979bcbd6e
David Drysdale <dmd@lurklurk.org>
2014-04-06 11:52:12 +0100

    Add more LocationMap tests

--- spec/locmap_test.js
+++ spec/locmap_test.js
@@ -39,6 +39,38 @@ describe("LocationMap", function() {
         expect(lm.find(2)).toEqual(12);
         expect(lm.find(6)).toEqual(22);
     });
+    it("should cope with duplicate inserts", function () {
+        var lm = new LocationMap();
+        lm.add(0, 10);
+        lm.add(1, 11);
+        lm.add(1, 11);
+        lm.add(1, 11);
+        lm.add(2, 12);
+        lm.add(3, 13);
+        lm.add(6, 22);
+        lm.add(6, 22);
+        lm.add(7, 23);
+        lm.add(7, 23);
+        expect(lm.find(2)).toEqual(12);
+        expect(lm.find(6)).toEqual(22);
+    });
+    it("should cope with changed inserts", function () {
+        var lm = new LocationMap();
+        lm.add(0, 10);
+        lm.add(1, 101);
+        lm.add(1, 102);
+        lm.add(1, 103);
+        lm.add(2, 12);
+        lm.add(3, 13);
+        lm.add(6, 22);
+        lm.add(6, 200);
+        lm.add(7, 23);
+        lm.add(7, 23);
+        // [0, 1) -> [10, 11); [1, 2) -> [103, 104); [2, 4) -> [12, 14); [6, 7) -> [200, 201); [7, 8) -> [23, 24)
+        expect(lm.find(1)).toEqual(103);
+        expect(lm.find(2)).toEqual(12);
+        expect(lm.find(6)).toEqual(200);
+    });
     it("should map sparse ranges", function () {
         var lm = new LocationMap();
         lm.add(0, 10);
@@ -68,6 +100,64 @@ describe("LocationMap", function() {
         expect(lm.findNext(123)).toEqual(-1);
         expect(lm.findNext(13)).toEqual(103);
         expect(lm.find(75)).toEqual(225);
+    });
+    it("should cope with duplicate range inserts", function () {
+        var lm = new LocationMap();
+        lm.addRange(10, 5, 100);
+        lm.addRange(50, 50, 200);
+        lm.add(122, 333);
+        lm.addRange(10, 5, 100);
+        lm.addRange(50, 50, 200);
+        expect(lm.findNext(123)).toEqual(-1);
+        expect(lm.findNext(13)).toEqual(103);
+        expect(lm.find(75)).toEqual(225);
+    });
+    it("should cope with changed range inserts", function () {
+        var lm = new LocationMap();
+        lm.addRange(10, 5, 100);
+        lm.addRange(50, 50, 200);
+        lm.add(122, 333);
+        lm.addRange(10, 5, 1000);
+        lm.addRange(50, 50, 2000);
+        // [10, 15) -> [1000, 1005); [50, 100) -> [2000, 2050); [122, 123) -> [333, 334)
+        expect(lm.findNext(123)).toEqual(-1);
+        expect(lm.findNext(13)).toEqual(1003);
+        expect(lm.find(75)).toEqual(2025);
+    });
+    it("should cope with contiguous range inserts", function () {
+        var lm = new LocationMap();
+        lm.addRange(10, 5, 100);
+        lm.addRange(15, 50, 105);
+        expect(lm.findNext(123)).toEqual(-1);
+        expect(lm.findNext(13)).toEqual(103);
+        expect(lm.find(15)).toEqual(105);
+        expect(lm.find(16)).toEqual(106);
+        expect(lm.find(64)).toEqual(154);
+        expect(lm.find(65)).toEqual(-1);
+    });
+    it("should cope with changed ranges becoming contiguous", function () {
+        var lm = new LocationMap();
+        lm.addRange(10, 5, 100);
+        lm.addRange(15, 50, 205);
+        lm.addRange(15, 20, 105);
+        // Now [10, 10+25) -> [100, 100+25), [35, 35+30) -> [225, 225+30)
+        expect(lm.findNext(123)).toEqual(-1);
+        expect(lm.findNext(13)).toEqual(103);
+        expect(lm.find(15)).toEqual(105);
+        expect(lm.find(16)).toEqual(106);
+        expect(lm.find(64)).toEqual(254);
+        expect(lm.find(65)).toEqual(-1);
+    });
+    it("should cope with inverted destinations", function () {
+        var lm = new LocationMap();
+        lm.addRange(10, 5, 1000);
+        lm.addRange(15, 50, 205);
+        expect(lm.findNext(123)).toEqual(-1);
+        expect(lm.findNext(13)).toEqual(1003);
+        expect(lm.find(15)).toEqual(205);
+        expect(lm.find(16)).toEqual(206);
+        expect(lm.find(64)).toEqual(254);
+        expect(lm.find(65)).toEqual(-1);
     });
     it("should merge maps with dest offset", function () {
         var lm = new LocationMap();
@@ -107,7 +197,10 @@ describe("LocationMap", function() {
         lm2.addRange(110, 10, 300);
         lm2.addRange(125, 5, 400);
 
+        // [0, 5) -> [100, 105); [10, 20) -> [120, 130) +
+        // [102, 104) -> [200, 202); [110, 120) -> [300, 310); [125, 130) -> [400,405)
         lm.compose(lm2);
+        // [2, 4) -> [200, 202); [15, 20) -> [400, 405)
         expect(lm.find(0)).toEqual(-1);
         expect(lm.find(2)).toEqual(200);
         expect(lm.find(3)).toEqual(201);
